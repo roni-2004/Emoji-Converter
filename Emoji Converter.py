67f1eb4e943c3fdb3778d49da2e4167d1ca26d8f
roni-2004 <jaronronialbert@gmail.com>
2022-09-04 15:03:23 +0530

    Initial Commit - Emoji Converter

---
+++ Emoji Converter.py
@@ -0,0 +1,21 @@
+from unittest import result
+
+
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😊",
+        ";)": "😏",
+        ":|": "😐",
+        "-_-": "😑",
+        ":O": "😲"
+    }
+    output = ""
+    for word in words:
+        output = output + emojis.get(word, word) + " "
+    return output
+
+
+message = input("> ") 
+result = emoji_converter(message)
+print(result)
